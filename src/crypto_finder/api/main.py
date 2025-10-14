7338e729027acf16e6e3b392c19470d1379223bd
AKN_007 <137386279+Anshuman122@users.noreply.github.com>
2025-10-14 11:31:26 +0530

    Update main.py

--- src/crypto_finder/api/main.py
+++ src/crypto_finder/api/main.py
@@ -1,19 +1,16 @@
-# Hinglish: API ka main entry point. Hum yahan apne endpoints router ko include kar rahe hain.
 
 from fastapi import FastAPI
 from crypto_finder.api import endpoints
 
-# FastAPI app ka object banao
 app = FastAPI(
     title="Crypto Finder API",
-    description="Firmware binaries me cryptographic primitives dhundhne ke liye API.",
+    description="API to fine cryptographic primitives in Firmware binaries",
     version="0.1.0"
 )
 
-# Endpoints router ko main app me include karo
 app.include_router(endpoints.router, prefix="/api/v1", tags=["Analysis"])
 
 @app.get("/", tags=["General"])
 def read_root():
-    """API ka welcome message."""
-    return {"message": "Welcome to Crypto Finder API! API documentation ke liye /docs par jayein."}
+
+    return {"message": "Welcome to Crypto Finder API! For API documentation go to  /docs}
